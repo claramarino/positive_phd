8b829f4299db817974cdf82d1d518c809123a6eb
Clara Marino <claramarino665@gmail.com>
2021-09-21 09:23:17 +0200

    wordcloud

--- 2_Analyses_tweets.R
+++ 2_Analyses_tweets.R
@@ -3,21 +3,24 @@ rm(list=ls())
 library(rtweet)
 library(dplyr)
 library(tidyr)
-library(RColorBrewer)
 library(ggplot2)
 library(tm)
 library(wordcloud)
-library(gganimate)
-library(SnowballC)
+library(reshape2)
+library(syuzhet)
+library(qdap)
+library(tidytext)
 
 
 # load tweets
 rt_phd_exp <- readRDS("Output/1_rt_phd_exp_2021-09-15")
 
-
 text = rt_phd_exp$text
 text[2]
 
+
+####---------------------PACKAGE tm ---------------------####
+# create a corpus, word freq, associations 
 
 tweets_source <- VectorSource(text)
 # Make a volatile corpus: tweets_corpus
@@ -25,7 +28,7 @@ tweets_corpus <- VCorpus(tweets_source)
 # Print out the tweets_corpus
 tweets_corpus
 
-####### Clean tweets ##########
+# Clean tweets
 clean_tw <- tm_map(tweets_corpus, removePunctuation)
 clean_tw <- tm_map(clean_tw, removeWords, stopwords("english"))
 clean_tw <- tm_map(clean_tw, stripWhitespace)
@@ -47,30 +50,69 @@ findAssocs(dtm, terms = "like", corlimit = 0.2)
 # remove sparse terms (terms occuring only in one document)
 dtm_high <- removeSparseTerms(dtm, 0.4)
 
-library(qdap)
 frequent_terms <- freq_terms(text, 100, stopwords = stopwords("english"))
 plot(frequent_terms[1:30,])
 
 fre = frequent_terms$FREQ/max(frequent_terms$FREQ)
 wordcloud(words = frequent_terms$WORD, freq = fre)
 
-####### Sentiments ##########
 
-library(tidytext)
+####--------------------- Sentiments ------------------------####
+
 tweets <- rt_phd_exp %>%
-  select(text) %>%
-  unnest_tokens(word, text)
+  select(text)
 tweet_words <- tweets %>%
+  unnest_tokens(word, text) %>%
   anti_join(stop_words)
 
-library(syuzhet)
+
+plain_text <- iconv(text, from="UTF-8", to="ASCII", sub="")
+plain_text_stm <- get_sentiment(plain_text)
+plain_text_nrc <- get_nrc_sentiment(plain_text)
+
+length(which(plain_text_stm<0))
+
+tweet_words %>%
+  filter(!(word %in% c("t.co","https","1","2","3","4","5","6",
+                       "phdvoice","phdchat","phdlife", "phdtips",
+                       "phdspeaks","phdstudent")))%>%
+  count(word) %>%
+  with(wordcloud(word, n, max.words = 100))
+
+tweet_words %>%
+  filter(!(word %in% c("t.co","https","1","2","3","4","5","6",
+                       "phdvoice","phdchat","phdlife", "phdtips",
+                       "phdspeaks","phdstudent"))) %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  acast(word ~ sentiment, value.var = "n", fill = 0) %>%
+  comparison.cloud(colors = c("firebrick", "chartreuse3"),
+                   max.words = 100)
+
+neg_pos_count <- tweet_words %>%
+  filter(!(word %in% c("t.co","https","1","2","3","4","5","6",
+                       "phdvoice","phdchat","phdlife", "phdtips",
+                       "phdspeaks","phdstudent"))) %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  acast(word ~ sentiment, value.var = "n", fill = 0)
+
+
+colSums(neg_pos_count)
+max(neg_pos_count[,1])
+max(neg_pos_count[,2])
+names(neg_pos_count)
+
+
+# using nrc but weird result----------------------------
+
 # Converting tweets to ASCII to trackle strange characters
 tweets <- iconv(tweets, from="UTF-8", to="ASCII", sub="")
 # removing retweets, in case needed 
 tweets <-gsub("(RT|via)((?:\\b\\w*@\\w+)+)","",tweets)
 # removing mentions, in case needed
 tweets <-gsub("@\\w+","",tweets)
-ew_sentiment<-get_nrc_sentiment((tweets))
+ew_sentiment<-get_nrc_sentiment(tweets)
 
 
 sentimentscores<-data.frame(colSums(ew_sentiment[,]))
@@ -84,5 +126,3 @@ ggplot(data=sentimentscores,aes(x=sentiment,y=Score))+
   xlab("Sentiments")+ylab("Scores")+
   ggtitle("Total sentiment based on scores")+
   theme_minimal()
-
-wordcloud(tweets_corpus, max.words = 100)
